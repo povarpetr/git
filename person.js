77672b7a8ae5dda1a17fe3f892569ec3f3c0074b
urrrrk <pomogitemnenaiti@mail.ru>
2022-10-24 20:20:57 +0300

    111

---
+++ person.js
@@ -0,0 +1,14 @@
+[
+   {
+      "guid":"c5983550-ca50-4e36-bd11-14ed1f65d644",
+      "personName":"Егор Плосков",
+      "email":"edp@renue.ru",
+      "city":"Екатеринбург"
+   },
+   {
+      "guid":"967b7a8d-b28b-4322-bc86-51bf14580594",
+      "personName":"Марина Кадочникова",
+      "email":"kmd@renue.ru",
+      "city":"Екатеринбург"
+   }
+]
